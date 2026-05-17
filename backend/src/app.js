fba8dd68075b0b13043a959547fcbe1e571c8cc8
Sindy138 <159185683+Sindy138@users.noreply.github.com>
2026-05-17 12:26:06 +0100

    Error correction in deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,7 +29,7 @@ app.use("/api/orders", ordersRoutes);
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("*", (req, res) => {
+app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
